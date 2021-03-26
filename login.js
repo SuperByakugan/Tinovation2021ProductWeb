893cbb4b771a55185c9fe382e50da58dc1231b2b
NishTheSpicyFish <55328240+NishTheSpicyFish@users.noreply.github.com>
2021-03-26 12:59:43 -0700

    Update login.js
    updated login var names and preferences

--- login.js
+++ login.js
@@ -3,28 +3,35 @@ var signedUp = false;
 
 
 function submitRegister(){
+  
     var email = document.getElementById("email").value; // not done
-    var password = document.getElementById("password").value; // not done
+    var password1 = document.getElementById("pwd").value; 
+    var password2 = document.getElementById("pwd2").value;
+  if(password1 != password2){
+    console.log(`Passwords don't match`);
+  }else{
+    console.log(`Email: ${email}\nPassword: ${password1}`);
+      firebase.auth().createUserWithEmailAndPassword(email, password1)
+    .then((userCredential) => {
+      // Signed in 
+      user = userCredential.user;
+      signedUp = true;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      // ..
+    });
+  }
     
-    console.log(`Email: ${email}\nPassword: ${password}`);
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in 
-    user = userCredential.user;
-    signedUp = true;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage);
-    // ..
-  });
+    
 }
 
 function submit(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+    var email = document.getElementById("login-email").value;
+    var password = document.getElementById("login-password").value;
     firebase.auth().signInWithEmailAndPassword(email, password)
   .then((userCredential) => {
     // Signed in
@@ -46,10 +53,12 @@ firebase.auth().onAuthStateChanged((user) => {
     var uid = user.uid;
     console.log(uid);
     if(signedUp){
-        var name = document.getElementById("name").value;
-        var preferences = document.getElementById("preferences").value;
-      
-        writeUserData(uid, name, preferences);
+        var firstName = document.getElementById("fname").value;
+        var lastName = document.getElementById("lname").value;
+        var vegetables = document.getElementById("vegetables").value;
+        var meat = document.getElementById("meat").value;
+        var dairy = document.getElementById("dairy").value;
+        writeUserData(uid, firstName, lastName, vegetables, meat, dairy);
         
     }
     // ...
@@ -69,9 +78,12 @@ function logOut(){
     });
 }
 
-function writeUserData(userId, name, preferences) {
+function writeUserData(userId, firstName, lastName, vegetables, meat, dairy) {
   firebase.database().ref('users/' + userId).set({
-    name: name,
-    preferences: preferences
+    firstName: firstName,
+    lastName: lastName,
+    vegetables: vegetables,
+    meat: meat,
+    dairy: dairy
   });
 }
