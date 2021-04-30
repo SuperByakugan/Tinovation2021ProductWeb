41aac82c954957e3b0feae9164a1e6656ae234e7
carefreepenguin <aaditya@live.com>
2021-04-30 13:05:55 -0700

    ChangeView fixed

--- javascript/inventory.js
+++
@@ -1,9 +0,0 @@
-function changeView(){
-    if(document.getElementById("Adder").style.display == "none"){
-        document.getElementById("Adder").style.display = "flex";
-        document.getElementById("SeeItems").style.display = "none";
-    }else{
-        document.getElementById("Adder").style.display = "none";
-        document.getElementById("SeeItems").style.display = "flex";    
-    }
-}
