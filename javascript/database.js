d18911101fc5e029d00cd2cc022c7925444fcb22
Tristan-Liu970 <55328241+Tristan-Liu970@users.noreply.github.com>
2021-05-21 13:15:10 -0700

    Add files via upload

--- javascript/database.js
+++ javascript/database.js
@@ -1,6 +1,252 @@
 var user;
 var uid;
 
+//getToken method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function getToken() {
+    //if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+    var email = localStorage.getItem("email");
+    var password = localStorage.getItem("password");
+	//testing
+	email = "foo@gmail.com";
+	//testing
+	password = "abc";
+    const info = {
+        "username": email,
+		"password": password
+    };
+
+    const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/getToken', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully got the token!')
+    } else {
+        alert('An error occurred while trying to get the token');
+    }
+}
+
+//getProfile method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function getProfile(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/getProfile', {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully got the profile!')
+    } else {
+        alert('An error occurred while trying to get the profile');
+    }
+}
+
+//updateProfile method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function updateProfile(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const u = firebase.auth().currentUser;
+	const data = {
+		"id": u.uid
+	}
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/updateProfile', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully updated the profile!')
+    } else {
+        alert('An error occurred while trying to update the profile');
+    }
+}
+
+//deleteProfile method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function deleteProfile(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/deleteProfile', {
+        method: 'DELETE',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully deleted the profile!')
+    } else {
+        alert('An error occurred while trying to delete the profile');
+    }
+}
+
+//updateItems method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function updateItems(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const u = firebase.auth().currentUser;
+	const data = {
+		"id": u.uid
+	}
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/updateItems', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully updated items!')
+    } else {
+        alert('An error occurred while trying to update the items');
+    }
+}
+
+//getInventory method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function getInventory(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/getInventory', {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully got the inventory!')
+    } else {
+        alert('An error occurred while trying to get the inventory');
+    }
+}
+
+//getCommunityRecipes method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function getCommunityRecipes(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/getCommunityRecipes', {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully got community recipes!')
+    } else {
+        alert('An error occurred while trying to get community recipes');
+    }
+}
+
+//postRecipes method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function postRecipes(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const u = firebase.auth().currentUser;
+	const data = {
+		"id": u.uid
+	}
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/postRecipes', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully posted recipes!')
+    } else {
+        alert('An error occurred while trying to post recipes');
+    }
+}
+
+//getRecipes method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function getRecipes(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/getRecipes', {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully got recipes!')
+    } else {
+        alert('An error occurred while trying to get recipes');
+    }
+}
+
+//likeRecipes method JSON RESULT IS ONLY PRINTED IN CONSOLE AS OF RIGHT NOW, NEED TO ACTUALLY GET INFORMATION FROM JSON
+async function likeRecipes(){
+	//if (localStorage.getItem("email") == null) return console.log("User is not logged in.");
+	const u = firebase.auth().currentUser;
+	const data = {
+		"id": u.uid
+	}
+	const res = await fetch('https://portablefridge-311105.wm.r.appspot.com/likeRecipes', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+			"Authorization": 'Bearer ${localStorage.getItem("token")}'
+        },
+        body: JSON.stringify(info),
+    });
+    console.log(res.ok);
+
+    if (res.ok) {
+        let json = await res.json();
+        console.log(json);
+        alert('Successfully liked recipes!')
+    } else {
+        alert('An error occurred while trying to like recipes');
+    }
+}
+
 //adds items to their list by getting the item from a field and quantity from a field and adding it
 //in the format of item: quantity in the JSON so quantities can be accesses using the item name
 async function addItem() {
