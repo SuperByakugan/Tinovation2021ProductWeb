4f1f60e4d1a1df2bacf8f6e3999381cd6dda50b8
Tetracyl <seifaziz10@gmail.com>
2021-07-02 10:46:54 -0700

    Initial Commit

--- javascript/inventory.js
+++
@@ -1,5 +0,0 @@
-function addItem() {
-    let div = document.createElement('div');
-    div = document.getElementById('item-card').cloneNode(true);
-    document.getElementById('items').appendChild(div);
-}
